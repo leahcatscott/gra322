842f34245477371c0808f382afdab1891c669822
leahcatscott <scott.leah.cat@gmail.com>
2023-02-28 14:52:50 -0700

    parallax

--- responsive-architects/js/scripts.js
+++ responsive-architects/js/scripts.js
@@ -4,3 +4,19 @@ $('.menu-button').click(function(){
 
 // Initialize Animate on Scroll
 AOS.init();
+
+// Initialize Jarallax
+$('.jarallax').jarallax({
+    speed: .2, 
+  });
+
+// Initialize Rellax
+
+var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+});
